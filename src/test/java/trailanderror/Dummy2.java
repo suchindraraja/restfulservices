f8ca2ca0438aaef08b97d4ca8dd8eb980eebff69
sukumar raja <sukumar raja@sukumar123>
2020-10-07 20:48:47 +0530

    dum2 pushing

---
+++ src/test/java/trailanderror/Dummy2.java
@@ -0,0 +1,13 @@
+package trailanderror;
+
+public class Dummy2 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("hello guru");
+
+	}
+
+}
---
+++ src/test/java/trailanderror/Dummy3.java
@@ -0,0 +1,13 @@
+package trailanderror;
+
+public class Dummy3 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("hello boss");
+
+	}
+
+}
